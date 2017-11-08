38a97afd224b568e54b1336c54bdf3fe2d7f7c65
Rich Wright <rchwrght@gmail.com>
2017-11-08 23:17:35 +0100

    intro to JS done

---
+++ JS Intro/script.js
@@ -0,0 +1,10 @@
+var firstName = prompt ("What is your first name?");
+var secondName = prompt("What is your second name?");
+var age = prompt("How old are you?");
+
+console.log("Your full name is " + firstName + " "+ secondName);
+console.log("You are " + age + " years old");
+
+var ageInDays = age * 365.25;
+
+alert("You are " + ageInDays + " days old");
